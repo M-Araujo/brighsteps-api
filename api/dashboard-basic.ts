c3bba6ef66db9bc8702c86d30acc24580f96e664
Miriam Araújo <miriamaraujoweb@gmail.com>
2025-05-23 11:20:48 +0100

    added dashboard multiple versions

---
+++ api/dashboard-basic.ts
@@ -0,0 +1,20 @@
+import { getRandomMovie } from "../lib/movies";
+import { getRandomMentor } from "../lib/mentors";
+import { getRandomTip } from "../lib/tips";
+import { goals } from "../lib/goalsAndHabits";
+
+export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  res.status(200).json({
+    movie: getRandomMovie(),
+    mentor: getRandomMentor(),
+    tip: getRandomTip()
+  });
+}
